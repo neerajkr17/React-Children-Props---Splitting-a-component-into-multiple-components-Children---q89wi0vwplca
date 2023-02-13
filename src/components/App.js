0d38f191abb169b3415bd8023c40f32f641f1ec3
Neeraj Kumar <121371264+neerajkr17@users.noreply.github.com>
2023-02-13 22:33:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,22 @@ import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
  //code here
+ const [input, setInput] = useState("");
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
 
   return (
     <div id="main">
-      <ChildComponent /> 
-       <input id='input' onChange={} />
+      <ChildComponent> 
+       <input id="input" onChange={handleInputChange} />
+       </ChildComponent>
     
-      <p id='output'>{}</p>
+      <p id="output">{Number(input)*Number(input)}</p>
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
